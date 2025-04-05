ef1d78249bba0f1db2aba38dc05dc19388d57425
irrmatii <itadserving@gmail.com>
2025-04-05 20:26:59 +0300

    testing

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 const ContactSection = () => {
     return (
-        <div id="Contact"  className='w-full h-screen absolute bottom-0 flex flex-col items-center bg-linear-to-b from-transparent via-transparent to-[#111]'>
+        <div id="Contact"
+             className='w-full h-screen absolute bottom-0 flex flex-col items-center bg-linear-to-b from-transparent via-transparent to-[#111]'>
             <div className="w-full flex justify-center pb-10 pt-5 relative">
-                <h1 className="text-[clamp(60px,calc(6vw+1rem),100px)] text-[#e3e2a9] text-shadow font-(family-name:--font-primary)">Contact Me</h1>
-                <h1 className="text-[clamp(50px,calc(2vw+1rem),100px)] font-extrabold text-shadow-white tracking-widest absolute bottom-11 z-40 font-(family-name:--font-secondary)">Contact Me</h1>
+                <h1 className="text-[clamp(60px,calc(6vw+1rem),100px)] text-[#e3e2a9] text-shadow font-(family-name:--font-primary)">Contact
+                    Me</h1>
+                <h1 className="text-[clamp(50px,calc(2vw+1rem),100px)] font-extrabold text-shadow-white tracking-widest absolute bottom-11 z-40 font-(family-name:--font-secondary)">Contact
+                    Me</h1>
             </div>
 
+            <h1>Test</h1>
             <div className="w-full h-[80%] flex items-center pb-10">
                 <form
                     className="flex flex-col items-center space-y-10 xl:space-y-20 w-[90%] max-w-[500px] xl:max-w-[700px] mx-auto"
